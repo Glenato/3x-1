83b0bfe078903ebeef36a4ee2223485c592a4c25
Glenato <124728975+Glenato@users.noreply.github.com>
2024-01-11 21:31:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def syntaxe():
 - run random : runs the 3x + 1 programme with a random number from 1 to 1,000,000
 - run forever : runs forever until you stop it or you find an other loop than 4, 2, 1
 - output false : stops saving the chart of the 3x + 1 programme in a png file, note that for the output to be saved chart must be activated (this is False by default)
-- chart false : stops showing a chart after the execution of the program 3x + 1 (this is True by default)
+- chart false : stops showing a chart after the execution of the program 3x + 1 (this is False by default)
 - info : get more information about the 3x + 1 program   
 - customodd + '4x + 1' : changes the value added to the odd numbers to whatever is after customodd (here is : 4x + 1)
 - customeven + '2' : changes the value added to the even numbers to whatever is after customeven (here is : 2)
